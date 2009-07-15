841c1f3e7e0e36284ea9cce95e5856d39cc9f378
Pelle Wessman <pelle@kodfabrik.se>
2009-07-15 14:22:04 +0200

    Main new feature is a working, but not complete, cron job

--- modules/fbconnector/fbconnector.js
+++ modules/fbconnector/fbconnector.js
@@ -4,7 +4,12 @@ function fbconnector_login() {
 
 FB.Bootstrap.requireFeatures(["Connect"], function() {
   FB.ensureInit(function () {
+    //Facebook wants us to log the user out of Facebook - but if we're in a very small window/iframe we can't show the UI so I found this in the facebook javascript
+    if (typeof FB.Connect._logoutMethod_noUI !== 'undefined') {
+      FB.Connect._logoutMethod = FB.Connect._logoutMethod_noUI;
+    }
     FB.Connect.logout();
   });
-  FB.init(Drupal.settings.fbconnector_api_key, Drupal.settings.fbconnector_xd_receiver);
+  //TODO: Make the popup configurable
+  FB.init(Drupal.settings.fbconnector_api_key, Drupal.settings.fbconnector_xd_receiver, {"forceBrowserPopupForLogin":true});
 });
